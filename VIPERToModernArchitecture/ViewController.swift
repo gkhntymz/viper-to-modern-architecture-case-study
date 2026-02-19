50f1dd5cbd0791b8a19701e7f30b7bd248ef5efd
gkhntymz <gokhantaymaz@yahoo.com>
2026-02-19 13:43:31 +0300

    Initial Commit

---
+++ VIPERToModernArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERToModernArchitecture
+//
+//  Created by Gökhan Taymaz on 19.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
